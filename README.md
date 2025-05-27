f89018d77fbec6805084c524f79c5674f3fb49c2
sourav-devops <sourav.shrestha22@gmail.com>
2025-05-27 11:08:22 +0530

    adding first file to repos and also updating the reeadme file.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python
 This is is my first project in github where i will try to learn python from begineer to pro level.
+All the best to me.
---
+++ prime.py
@@ -0,0 +1,18 @@
+
+x = int(input("Enter  number : "))
+z=1
+if x==1:
+        print(" 1 is neither prime nor composite")
+elif x==2 or x == 3:
+     print ("prime")
+else:
+    for i in range(2,int(x**0.5)+1):
+        if x%i == 0:
+            print("not prime")
+            z=0
+            break
+
+    if z == 1:
+        print("prime")       
+
+
